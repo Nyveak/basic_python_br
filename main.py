b44e8c57e6e3e0cc057e8b9f11a8816ac631dbc4
Nyveak <k.igames43@gmail.com>
2026-02-24 20:59:12 +0000

    Punto 1-4: Variables, suma, par y recorrido de lista

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+
+app = FastAPI()
+
+@app.get("/")
+def initialEndpoint():
+	return {"message":"Hola Mundo"}
+
+#Exercise 1
+
+@app.get("/exercise1")
+def exercise1():
+	nombre = "Kevin"
+	return {"exercise": 1, "value": nombre}
+
+#Exercise 2
+
+@app.get("/exercise2")
+def exercise2(a: int = 0, b: int = 0):
+	return {"exercise": 2, "resultado": a + b}
+
+#Exercise 3
+
+@app.get("/exercise3")
+def exercise3(num: int):
+	return {"exercise": 3, "numero": num, "es par": num % 2 == 0} 
+
+#Exercise 4
+
+@app.get("/exercise4")
+def exercise4():
+	lista = [1, 2, 3, 4, 5]
+	recorrido = [n for n in lista]
+	return {"exercise": 4, "Lista": recorrido}
